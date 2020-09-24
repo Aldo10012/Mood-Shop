6941e9fa94b29288e759163ddf6e23aeaca7a86c
Alberto <aldo10012@gmail.com>
2020-09-23 20:31:51 -0700

    redid vids 1 to 9

--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -33,142 +33,235 @@ for (let i=0; i<data.length; ++i) {
     // put new div inside items container
     itemsContainer.appendChild(newDiv)
   }
+const all_items_button = Array.from(document.querySelectorAll("button"))
+all_items_button.forEach(elt => elt.addEventListener('click', () => {
+    addItem(elt.getAttribute('id'), elt.getAttribute('data-price'))
+    showItems()
+  }))
+console.log(all_items_button)
 
-
-// --------------------------------------------------------------
-// handle change events on update input
-itemList.onchange = function(e){
-    if(e.target && e.target.classList.containe('update')){
-        const name = e.target.dataset.name
-        const qty = parseInt(e.target.value)
-        updateCart(name,qty)
-    }
-}
-// --------------------------------------------------------------
-// handle clicks on list
-itemList.onclick = function(e){
-    if(e.target && e.target.classList.contains('remove')){
-        const name = e.target.dataset.name
-        removeItem(name)
-    } else if(e.target && e.target.classList.contains('add-one')) {
-        const name = e.target.dataset.name
-        addItem(name)
-    } else if(e.target && e.target.classList.contains('remove-one')) {
-        const name = e.target.dataset.name
-        removeItem(name,1)
-    }
-}
-// --------------------------------------------------------------
-// handle add form submit
-addForm.onsubmit = function(e){
-    e.preventDefault()
-    const name = itemName.value
-    const price = itemPrice.value
-    addItem(name,price)
-}
-// --------------------------------------------------------------
-// add item  
 const cart = []
-function addItem(name,price){
-    for (let i = 0; i < cart.length; i+=1) {
-        if (cart[i].name === name) {
+
+// Add Items ------------------------------------------------------
+function addItem(name, price){
+    for(let i = 0; i < cart.length; i += 1) {
+        if(cart[i].name === name) {
             cart[i].qty += 1
+            return
         }
-        return
     }
     const item = {name, price, qty:1}
     cart.push(item)
 }
-// --------------------------------------------------------------
-// show item  
-function showItems(){
-    let qty = getQty()
-    let total = getTotal()
-    //console.log(`You have ${qty} items in your cart. `)
-    cartQty.innerHTML = `You have ${qty} items in your cart. `
 
+// Show Items ------------------------------------------------------
+function showItems() {
+    const qty = getQty()
+    const total = getTotal()
+    //console.log(`You have ${qty} items in your cart`)
+    cartQty.innerHTML = `You have ${qty} items in your cart`
+    
     let itemStr = ''
-    for (let i = 0; i < cart.length; i += 1) {
-        //console.log(`${cart[i].name} $${cart[i].price} ${cart[i].qty}`)
+    for(let i = 0; i < cart.length; i += 1) {
+        //console.log(`- ${cart[i].name} ${cart[i].price} x ${cart[i].qty}`)
         const {name, price, qty} = cart[i]
-        itemStr += `<li>
-            ${name} $${price} x ${qty} = ${price * qty}
-            <button class= 'remove' data-name= '${name}'> Remove </button>
-            <button class= 'add-one' data-name= '${name}'> + </button>
-            <button class= 'remove-one' data-name= '${name}'> - </button>
-            <input class='update' type='number' data-name='${name}'>
-        </li>` 
+        itemStr +=  `<li>${name} ${price} x ${qty} = $${price * qty}</li>`
     }
-    const all_items_button = Array.from(document.querySelectorAll("button"))
-
     itemList.innerHTML = itemStr
-    //console.log(`Total in cart is ${total.toFixed(2)}`)  
-    cartTotal.innerHTML = `Total in cart is ${total.toFixed(2)}`
+
+    //console.log(`Tatal in cart: $${total}`)
+    cartTotal.innerHTML = `Tatal in cart: $${total}`
 }
-// --------------------------------------------------------------
-// get quantity  
-function getQty() {
-    let qty = 1
-    for (let i = 0; i < cart.length; i += 1) {
-        qty += cart[i].qty
-        all_items_button.forEach(elt => elt.addEventListener('click', () => {
-            addItem(elt.getAttribute('id'), elt.getAttribute('data-price'))
-            showItems()
-          }))
+
+// Get Qty ------------------------------------------------------
+function getQty(){
+    let qty = 0
+    for(let i = 0; i < cart.length; i += 1) {
+        qty += cart[i].qty  
     }
     return qty
 }
-// --------------------------------------------------------------
-// get total  
-function getTotal() {
-    let total = 0
-    for (let i = 0; i < cart.length; i += 1) {
-        total = cart[i].price * cart[i].qty
-        all_items_button.forEach(elt => elt.addEventListener('click', () => {
-            addItem(elt.getAttribute('id'), elt.getAttribute('data-price'))
-            showItems()
-          }))
-    }
-    return total
-}
-// --------------------------------------------------------------
-// remove item  
-function removeItem(name, qty = 0){
-    for (let i = 0; i < cart.length; i += 1) {
-        all_items_button.forEach(elt => elt.addEventListener('click', () => {
-            addItem(elt.getAttribute('id'), elt.getAttribute('data-price'))
-            showItems()
-          }))
-        if (cart[i].name === name) {
-            if (qty > 0) {
-                cart[i].qty -= qty
-            }
-            if (cart[i].qty < 1 || qty == 0){
-                cart.splice(i, 1)
-            }
-            showItems()
-            return
-        }
-    }
-}
-// --------------------------------------------------------------
-function pdateCart(name,qty) {
+
+// Get Total ------------------------------------------------------
+function getTotal(){
+    let total = 0 
     for(let i = 0; i < cart.length; i += 1) {
-        if(cart[i].name == name) {
-            if(qty < 1) {
-                removeItem(name)
-                return
-            }
-            cart[i].qty = qty
-            showItems()
+        total += cart[i].price * cart[i].qty
+    }
+    return total.toFixed(2)
+}
+
+// Remove Item ------------------------------------------------------
+function removeItem(name, qty=0) {
+    for(let i = 0; i < cart.length; i += 1) {
+        if(cart[i].name === name) {
+            if(qty > 0){
+                cart[i].qty -= qty
+            } 
+            if(cart[i].qty < 1 || qty === 0){
+                cart.splice(i,1)
+            }       
             return
         }
     }
 }
 
+addItem('apple',0.99)
+addItem('apple',0.99)
+addItem('orange',1.39)
+addItem('mango',0.02)
+addItem('orange',1.39)
+addItem('apple',0.99)
 
-
-addItem('apple',1)
-addItem('orange',2)
-addItem('mango',3)
 showItems()
+removeItem('apple',1)
+removeItem('mango')
+showItems()
+
+
+
+
+
+
+
+
+
+
+
+// // --------------------------------------------------------------
+// // handle change events on update input
+// itemList.onchange = function(e){
+//     if(e.target && e.target.classList.containe('update')){
+//         const name = e.target.dataset.name
+//         const qty = parseInt(e.target.value)
+//         updateCart(name,qty)
+//     }
+// }
+// // --------------------------------------------------------------
+// // handle clicks on list
+// itemList.onclick = function(e){
+//     if(e.target && e.target.classList.contains('remove')){
+//         const name = e.target.dataset.name
+//         removeItem(name)
+//     } else if(e.target && e.target.classList.contains('add-one')) {
+//         const name = e.target.dataset.name
+//         addItem(name)
+//     } else if(e.target && e.target.classList.contains('remove-one')) {
+//         const name = e.target.dataset.name
+//         removeItem(name,1)
+//     }
+// }
+// // --------------------------------------------------------------
+// // handle add form submit
+// addForm.onsubmit = function(e){
+//     e.preventDefault()
+//     const name = itemName.value
+//     const price = itemPrice.value
+//     addItem(name,price)
+// }
+// // --------------------------------------------------------------
+// // add item  
+// const cart = []
+// function addItem(name,price){
+//     for (let i = 0; i < cart.length; i+=1) {
+//         if (cart[i].name === name) {
+//             cart[i].qty += 1
+//         }
+//         return
+//     }
+//     const item = {name, price, qty:1}
+//     cart.push(item)
+// }
+// // --------------------------------------------------------------
+// // show item  
+// function showItems(){
+//     let qty = getQty()
+//     let total = getTotal()
+//     //console.log(`You have ${qty} items in your cart. `)
+//     cartQty.innerHTML = `You have ${qty} items in your cart. `
+
+//     let itemStr = ''
+//     for (let i = 0; i < cart.length; i += 1) {
+//         //console.log(`${cart[i].name} $${cart[i].price} ${cart[i].qty}`)
+//         const {name, price, qty} = cart[i]
+//         itemStr += `<li>
+//             ${name} $${price} x ${qty} = ${price * qty}
+//             <button class= 'remove' data-name= '${name}'> Remove </button>
+//             <button class= 'add-one' data-name= '${name}'> + </button>
+//             <button class= 'remove-one' data-name= '${name}'> - </button>
+//             <input class='update' type='number' data-name='${name}'>
+//         </li>` 
+//     }
+//     const all_items_button = Array.from(document.querySelectorAll("button"))
+
+//     itemList.innerHTML = itemStr
+//     //console.log(`Total in cart is ${total.toFixed(2)}`)  
+//     cartTotal.innerHTML = `Total in cart is ${total.toFixed(2)}`
+// }
+// // --------------------------------------------------------------
+// // get quantity  
+// function getQty() {
+//     let qty = 1
+//     for (let i = 0; i < cart.length; i += 1) {
+//         qty += cart[i].qty
+//         all_items_button.forEach(elt => elt.addEventListener('click', () => {
+//             addItem(elt.getAttribute('id'), elt.getAttribute('data-price'))
+//             showItems()
+//           }))
+//     }
+//     return qty
+// }
+// // --------------------------------------------------------------
+// // get total  
+// function getTotal() {
+//     let total = 0
+//     for (let i = 0; i < cart.length; i += 1) {
+//         total = cart[i].price * cart[i].qty
+//         all_items_button.forEach(elt => elt.addEventListener('click', () => {
+//             addItem(elt.getAttribute('id'), elt.getAttribute('data-price'))
+//             showItems()
+//           }))
+//     }
+//     return total
+// }
+// // --------------------------------------------------------------
+// // remove item  
+// function removeItem(name, qty = 0){
+//     for (let i = 0; i < cart.length; i += 1) {
+//         all_items_button.forEach(elt => elt.addEventListener('click', () => {
+//             addItem(elt.getAttribute('id'), elt.getAttribute('data-price'))
+//             showItems()
+//           }))
+//         if (cart[i].name === name) {
+//             if (qty > 0) {
+//                 cart[i].qty -= qty
+//             }
+//             if (cart[i].qty < 1 || qty == 0){
+//                 cart.splice(i, 1)
+//             }
+//             showItems()
+//             return
+//         }
+//     }
+// }
+// // --------------------------------------------------------------
+// function pdateCart(name,qty) {
+//     for(let i = 0; i < cart.length; i += 1) {
+//         if(cart[i].name == name) {
+//             if(qty < 1) {
+//                 removeItem(name)
+//                 return
+//             }
+//             cart[i].qty = qty
+//             showItems()
+//             return
+//         }
+//     }
+// }
+
+
+
+// addItem('apple',1)
+// addItem('orange',2)
+// addItem('mango',3)
+// showItems()
